cb37531edec0d8bc1817631b4ab0e48388a9f928
Stanislav Dimov <sdimov@abv.bg>
2021-05-24 18:02:21 +0300

    repair server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 
 app.get('*', (req, res) =>
-    res.sendFile('app.component.html', { root: path.join(__dirname, '') }),
+    res.sendFile('app.component.html', { root: path.join('/src',__dirname) }),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8082);
