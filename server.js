df3a7a37837b3058b40b13d54c70ea0a48af255f
Stanislav Dimov <sdimov@abv.bg>
2021-05-24 18:15:53 +0300

    repair server.js

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
+app.use(express.static('./dist/postinfo'));
 
-app.get('*', (req, res) =>
-    res.sendFile('app.component.html', { root: path.join('/src',__dirname) }),
+app.get('/*', (req, res) =>
+    res.sendFile('app.component.html', {root: 'dist/postinfo/app'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8082);
+app.listen(process.env.PORT || 8080);
