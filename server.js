ab6ccfdd71e6bb42432cf0b0bf7a7968ce1a8b33
Stanislav Dimov <sdimov@abv.bg>
2021-05-24 17:19:13 +0300

    change main page

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('./src/app/giapp.component.html'),
 );
 
 // Start the app by listening on the default Heroku port
